54b01fc874a04bb4b9722d441f5baa6e50399ce0
Justin Capalbo <capalbo.justin@gmail.com>
2018-01-27 03:39:01 -0500

    Add Transmission model

---
+++ Assets/Scripts/Transmissions/Receiver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[RequireComponent(typeof(Repository))]
+public class Receiver : MonoBehaviour
+{
+    //Transmissions I am allowed to receive
+    [SerializeField]
+    private List<TransmissionType> ValidTransmissions = new List<TransmissionType>();
+
+    public Repository Repository;
+
+    public void Awake()
+    {
+        Repository = GetComponent<Repository>();
+    }
+
+    //Determine if I am allowed to receive this transmission
+    public bool CanReceive(TransmissionType t)
+    {
+        return ValidTransmissions.Contains(t);
+    }
+
+    //If I already have this transmission, don't receive it
+    public bool TryReceive(TransmissionType t)
+    {
+        if (Repository.Contains(t))
+            return false;
+
+        Repository.Add(t);
+        return true;
+    }
+}
+
