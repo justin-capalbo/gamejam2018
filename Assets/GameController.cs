88d494a2bb230728d06110d504416f0ccff4d134
Matthew Cattron <oakwood@gmail.com>
2018-01-27 13:59:39 -0500

    - added 4 levels - added game controller that changes levels - added player spawn point prefab - added goal prefab that goes to next level

---
+++ Assets/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadLevel(string name) {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private float yOffset = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(player,new Vector3(transform.position.x,transform.position.y+yOffset,0f),Quaternion.identity);		
+	}
+}
