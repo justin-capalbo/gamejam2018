abbe9b26ac0d9e95de3b7d28f440d91261dd9df6
Matthew Cattron <oakwood@gmail.com>
2018-01-28 12:01:14 -0500

    sdasdfasd

---
+++ Assets/KillMe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillMe : MonoBehaviour {
+
+    float timer = 0f;
+    float maxTime = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= maxTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
